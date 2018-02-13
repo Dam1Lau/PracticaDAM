4d3bf79b4c4132e205a47405098d816208bcd68e
dam1 <dam1@1DAM-04.ies.quevedo>
2018-02-13 12:19:22 +0100

    Segundo Cambio de Prueba

--- Ajedrez/src/ajedrez/Ajedrez.java
+++ Ajedrez/src/ajedrez/Ajedrez.java
@@ -17,6 +17,7 @@ public class Ajedrez {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Prueba numero Uno");
+        System.out.println("Prueba rama nueva");
     }
     
 }
