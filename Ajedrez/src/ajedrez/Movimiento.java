30738ec64b0636fbe8dc8b71a56a55d186fc1666
dam1 <dam1@1DAM-04.ies.quevedo>
2018-02-13 13:10:25 +0100

    Primeras clases creadas

---
+++ Ajedrez/src/ajedrez/Movimiento.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ajedrez;
+
+/**
+ *
+ * @author dam1
+ */
+public class Movimiento {
+    private Posicion posInicial;
+    private Posicion posFinal;
+    
+    //construct
+    
+    public Movimiento() {
+    }
+    public Movimiento(Posicion posInicial, Posicion posFinal) {
+        this.posInicial = posInicial;
+        this.posFinal = posFinal;
+    }
+    
+    //get set
+    
+    
+    
+}
