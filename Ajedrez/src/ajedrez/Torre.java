529dad5a0b4c898f0d10e606a5ef1823e3acf41a
Laura <Laura@Laura-PC>
2018-02-13 19:42:43 +0100

    Piezas añadidas Solo printean

---
+++ Ajedrez/src/ajedrez/Torre.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ajedrez;
+
+/**
+ *
+ * @author Laura
+ */
+public class Torre extends Piezas {
+    //constructores
+    
+    public Torre() {
+        nombre = "Torre";
+        color = "Blanca";
+    }
+
+    public Torre(String color, String nombre) {
+        super(color, nombre);
+    }
+    
+    //Métodos
+    
+    @Override
+    public boolean puedeMoverse(){
+        boolean movimiento=false;
+    
+        
+        return movimiento;
+    }
+    
+    @Override
+    public void pintarPieza(){
+        if ("Blanca".equals(this.color))
+        System.out.println("\u2656");
+        else
+            System.out.println("\u265C");
+    }
+
+
+}
